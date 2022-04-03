ebc473dfece8c0e7adc5bfc552ebdcccef319235
Румата Эсторский <valera.dt@gmail.com>
2022-04-03 13:29:07 +0200

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,10 +4,10 @@ object Dependencies {
 
   val logging = Seq(
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4",
-    "ch.qos.logback" % "logback-classic" % "1.2.10"
+    "ch.qos.logback" % "logback-classic" % "1.2.11"
   )
 
-  val specs2Version = "4.14.0"
+  val specs2Version = "4.15.0"
   val specs2 = Seq(
     "org.specs2" %% "specs2-core" % specs2Version % "test"
   )
